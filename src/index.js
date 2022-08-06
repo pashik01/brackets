53ca5e8150265fd3e8ed7a2337508e38299c2def
pashik01 <105845661+pashik01@users.noreply.github.com>
2022-08-06 16:20:52 +0300

    feat:add js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) return false;
+  let strLengthOld = str.length;
+  let strLengthNew = '';
+  while (strLengthNew !== strLengthOld) {
+    strLengthOld = str.length;
+    bracketsConfig.forEach(bracketPair => {
+      bracketPair = bracketPair.join('');
+      while (str.indexOf(bracketPair) >= 0) {
+      str = str.replace(bracketPair,'');
+      }     
+    }) 
+    strLengthNew = str.length;
+  }  
+  if (str.length > 0) return false  
+  return true   
 }
